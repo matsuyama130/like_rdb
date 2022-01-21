5153e98c8eaf61fec54a20dcb5ce2d8c8d88345a
matsuyama130 <matsuyama130@gmail.com>
2022-01-21 11:04:20 +0900

    sqlupdate

--- lost_factory.sql
+++ lost_factory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2022-01-13 12:33:08
+-- 生成日時: 2022-01-21 02:56:13
 -- サーバのバージョン： 10.4.22-MariaDB
 -- PHP のバージョン: 8.0.13
 
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- テーブルの構造 `check_table`
+--
+
+CREATE TABLE `check_table` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `lost_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- テーブルのデータのダンプ `check_table`
+--
+
+INSERT INTO `check_table` (`id`, `user_id`, `lost_id`, `created_at`) VALUES
+(14, 5, 10, '2022-01-21 10:34:57'),
+(15, 5, 6, '2022-01-21 10:35:17'),
+(16, 5, 11, '2022-01-21 10:35:17'),
+(17, 5, 4, '2022-01-21 10:35:18'),
+(18, 5, 9, '2022-01-21 10:35:18'),
+(23, 5, 2, '2022-01-21 10:43:52'),
+(25, 5, 8, '2022-01-21 10:51:17'),
+(27, 5, 7, '2022-01-21 10:51:39');
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `lost_table`
 --
 
@@ -32,17 +59,28 @@ CREATE TABLE `lost_table` (
   `tool` varchar(128) COLLATE utf8mb4_bin NOT NULL,
   `takeout` date NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
+  `updated_at` datetime NOT NULL,
+  `waypoint1` varchar(10) COLLATE utf8mb4_bin NOT NULL,
+  `waypoint2` varchar(10) COLLATE utf8mb4_bin NOT NULL,
+  `waypoint3` varchar(10) COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- テーブルのデータのダンプ `lost_table`
 --
 
-INSERT INTO `lost_table` (`id`, `tool`, `takeout`, `created_at`, `updated_at`) VALUES
-(2, 'マイクロ', '2022-01-12', '2022-01-12 19:10:55', '2022-01-12 19:10:55'),
-(3, 'ノギス', '2022-01-13', '2022-01-12 19:29:50', '2022-01-12 19:29:50'),
-(4, '三菱', '2022-01-26', '2022-01-12 19:52:56', '2022-01-12 19:52:56');
+INSERT INTO `lost_table` (`id`, `tool`, `takeout`, `created_at`, `updated_at`, `waypoint1`, `waypoint2`, `waypoint3`) VALUES
+(2, 'マイクロ', '2022-01-12', '2022-01-12 19:10:55', '2022-01-12 19:10:55', '', '', ''),
+(3, 'ノギス', '2022-01-13', '2022-01-12 19:29:50', '2022-01-12 19:29:50', '', '', ''),
+(4, '三菱', '2022-01-26', '2022-01-12 19:52:56', '2022-01-12 19:52:56', '', '', ''),
+(6, 'aaa', '2022-01-15', '2022-01-14 16:59:22', '2022-01-14 16:59:22', '', '', ''),
+(7, 'aaa', '2022-01-06', '2022-01-14 17:00:37', '2022-01-14 17:00:37', '', '', ''),
+(8, 'aaaaaa', '2022-01-01', '2022-01-14 17:02:04', '2022-01-14 17:02:04', '', '', ''),
+(9, 'a', '2022-01-30', '2022-01-14 17:05:06', '2022-01-14 17:05:06', '', '', ''),
+(10, 'aaaa', '2022-01-12', '2022-01-14 17:06:46', '2022-01-14 17:06:46', '', '', ''),
+(11, 'aaa', '2022-01-20', '2022-01-14 17:08:23', '2022-01-14 17:08:23', '', '', ''),
+(12, 'eeee', '2022-01-13', '2022-01-14 17:09:18', '2022-01-14 17:09:18', '', '', ''),
+(13, 'aa', '2022-01-15', '2022-01-20 15:45:50', '2022-01-20 15:45:50', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -76,6 +114,12 @@ INSERT INTO `users_table` (`id`, `username`, `password`, `is_admin`, `is_deleted
 --
 
 --
+-- テーブルのインデックス `check_table`
+--
+ALTER TABLE `check_table`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- テーブルのインデックス `lost_table`
 --
 ALTER TABLE `lost_table`
@@ -92,10 +136,16 @@ ALTER TABLE `users_table`
 --
 
 --
+-- テーブルの AUTO_INCREMENT `check_table`
+--
+ALTER TABLE `check_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
 -- テーブルの AUTO_INCREMENT `lost_table`
 --
 ALTER TABLE `lost_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- テーブルの AUTO_INCREMENT `users_table`
